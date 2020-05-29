d27b8476c65646557439504e9e2993bdf4982001
MB6718 <mb6718@mail.ru>
2020-05-29 05:10:06 +0500

    feat(src/app.py, src/db.py): Добавлен функционал
    - Добавлена обработка пути для главной страницы
    - Добавлен шаблон главной страницы
    - Добавлена обработка регистрации новго пользователя
    - Добавлена обработка получения списка городов
    - Добавлена обработка регистрации нового города
    - Добавлена обработка получения списка объявлений
    - Написана логика работы с БД. Функции get_db и close_db
    
    new(logo/as-loga.png): Добавлен логотип сайта
    
    refrеsh(database.db): Оптимизировал и сжал тестовую БД

--- src/app.py
+++ src/app.py
@@ -1,14 +1,20 @@
-from flask import Flask, request, session, jsonify
+from flask import Flask, request, session, jsonify, render_template
+import datetime
 from db import get_db, close_db
 
-app = Flask(__name__) 
+app = Flask(__name__, static_folder='templates\static')
 app.teardown_appcontext(close_db)
 
 @app.route('/')
 @app.route('/index')
 def index():
 	""" Обработка запроса главной страницы нашего сайта объявлений """
-	return '', 200
+	now = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+	return render_template(
+		'index.html',
+		app_title='Auto Sales',
+		date_time_stamp=now
+	)
 
 @app.route('/auth/login', methods=["POST"])
 def login():
@@ -23,14 +29,150 @@ def logout():
 @app.route('/users', methods=["POST"])
 def users():
 	""" Обработка регистрации нового пользователя"""
-	pass
+	# получаем первостепенные поля из структуры JSON запроса
+	request_json = request.json
+	email = request_json.get('email')
+	password = request_json.get('password')
+	first_name = request_json.get('first_name')
+	last_name = request_json.get('last_name')
+	
+	# если хотя бы одно поле пусто или не определено, возвращаем код 400
+	if not email or not password or not first_name or not last_name:
+		return '', 400
+
+	# создаём соединение с БД
+	con = get_db()
+	
+	# email - является уникальным ИД пользователя при регистрации
+	# означает что не может существовать 2 пользователя с одним email адресом !
+	# а значит проверим, зарегистрирован ли в БД такой e-mail
+	cur = con.execute(f"""
+		SELECT ac.email
+		FROM account as ac
+		WHERE ac.email IN ('{email}')
+		"""
+	)
+	result = cur.fetchone()
+	
+	# и если он уже существует, возвращаем код 409
+	if result is not None:
+		return '', 409
+
+	# проверяем, отмечен ли регистрируемый пользователь как "продавец"
+	if request_json.get('is_seller'):
+		# и если отмечен, то получаем необходимые для "продавца" поля 
+		# из структуры JSON запроса
+		phone = request_json.get('phone')
+		zip_code = request_json.get('zip_code')
+		city_id = request_json.get('city_id')
+		street = request_json.get('street')
+		home = request_json.get('home')
+		
+		# если хотя бы одно поле пусто или не определено, возвращаем код 400
+		if not phone or not zip_code or not city_id or not street or not home:
+			return '', 400
+			
+		# регистрируем (с записью в БД) пользователя как продавца
+		cur = con.execute("""
+			INSERT INTO account (first_name,
+								 last_name,
+								 email,
+								 password)
+			VALUES (?, ?, ?, ?)
+			""",
+			(first_name, last_name, email, password)
+		)
+		con.commit()
+		
+		# получаем id зарегистрированного пользователя
+		cur = con.execute(f"""
+			SELECT ac.id as id
+			FROM account as ac
+			WHERE ac.email = '{email}'
+			"""
+		)
+		result = cur.fetchone()['id']
+		
+		# регистрируем почтовый индекс и связываем с указанным ID города
+		cur = con.execute("""
+			INSERT OR IGNORE INTO zipcode (zip_code, city_id)
+			VALUES (?, ?)
+			""",
+			(zip_code, city_id,)
+		)
+		con.commit()
+		
+		# добавляем нового продавца и привязываем его к аккаунту пользователя
+		cur = con.execute("""
+			INSERT INTO seller (zip_code, street, home, phone, account_id)
+			VALUES (?, ?, ?, ?, ?)
+			""",
+			(zip_code, street, home, phone, int(result))
+		)
+		con.commit()
+		
+		# если успех, возвращаем ответ в виде JSON объекта
+		cur = con.execute(f"""
+			SELECT ac.id, ac.first_name, ac.last_name, ac.email, ac.password,
+				   slr.zip_code, slr.street, slr.home, slr.phone,
+				   zc.city_id
+			FROM account as ac
+				JOIN seller as slr ON slr.account_id = ac.id
+				JOIN zipcode as zc ON zc.zip_code = slr.zip_code
+			WHERE ac.email = '{email}'
+			"""
+		)
+		result = cur.fetchall()
+		
+		result = [dict(row) for row in result]
+		result = result[0]
+		result['is_seller'] = 'true'
+		
+		return jsonify([result])
+	else:
+		# иначе, регистрируем (с записью в БД) простого пользователя
+		cur = con.execute("""
+			INSERT INTO account (first_name,
+								 last_name,
+								 email,
+								 password)
+			VALUES (?, ?, ?, ?)
+			""",
+			(first_name, last_name, email, password)
+		)
+		con.commit()
+		
+		# если успех, возвращаем ответ в виде JSON объекта
+		cur = con.execute(f"""
+			SELECT *
+			FROM account as ac
+			WHERE ac.email = '{email}'
+			"""
+		)
+		result = cur.fetchall()
+		
+		result = [dict(row) for row in result]
+		result = result[0]
+		result['is_seller'] = 'false'
+		
+		return jsonify([result])
 
 @app.route('/users/<int:id>', methods=["GET", "PATCH"])
 def users_id():
 	""" Обработка получения сведений о пользователе по его ID
 		Обработка частичного редактирования сведений о пользователе по его ID
 	"""
-	pass
+	# if request.method=='GET':
+		# con = get_db()
+		# cur = con.execute("""
+			# SELECT *
+			# FROM account
+			# WHERE account.id = 1
+			# """
+		# )
+		# result = cur.fetchone()
+		
+		# return jsonify([dict(row) for row in result])
 
 @app.route('/users/<int:id>/ads', methods=["GET", "POST"])
 def users_ads():
@@ -44,7 +186,17 @@ def ads():
 	""" Обработка получения всех объявлений
 		Обработка добавления нового объявления на сайт
 	"""
-	pass
+	# получаем список всех объявлений
+	if request.method=='GET':
+		con = get_db()
+		cur = con.execute("""
+			SELECT * 
+			FROM ad
+			"""
+		)
+		result = cur.fetchall()
+		
+		return jsonify([dict(row) for row in result])
 
 @app.route('/ads/<int:id>', methods=["GET", "DELETE", "PATCH"])
 def ads_id():
@@ -59,7 +211,60 @@ def cities():
 	""" Обработка получения списка всех городов из БД
 		Обработка добавления в БД нового города
 	"""
-	pass
+	# при GET запросе выдадим JSON список городов
+	if request.method=='GET':
+		con = get_db()
+		cur = con.execute("""
+			SELECT *
+			FROM city
+			"""
+		)
+		result = cur.fetchall()
+
+		return jsonify([dict(row) for row in result])
+	
+	# при POST запросе добавим новый город, если он не существует,
+	# иначе вернём его
+	if request.method=='POST':
+		# получаем поле name из структуры JSON запроса c именем города
+		request_json = request.json
+		name = request_json.get('name')
+		
+		# если поле name не определено, возвращаем код 400
+		if not name:
+			return '', 400
+		
+		# создаём соединение с БД
+		con = get_db()
+		
+		# проверяем, существует ли такой город в БД
+		cur = con.execute(f"""
+			SELECT *
+			FROM city as c
+			WHERE c.name = '{name}'
+			"""
+		)
+		result = cur.fetchall()
+
+		# если город отсутствует в БД, добавляем его
+		if not result:
+			cur = con.execute("""
+				INSERT INTO city (name)
+				VALUES (?)
+				""",
+				(name,)
+			)
+			con.commit()
+
+			cur = con.execute(f"""
+				SELECT *
+				FROM city as c
+				WHERE c.name = '{name}'
+				"""
+			)
+			result = cur.fetchall()
+		
+		return jsonify([dict(row) for row in result])
 
 @app.route('/colors', methods=["GET", "POST"])
 def colors():
--- src/db.py
+++ src/db.py
@@ -2,10 +2,20 @@ import sqlite3
 
 from flask import g
 
+DB_FILE='database.db'
+
 def get_db():
 	""" Получить данные из БД """
-	pass
+	if 'db' not in g:
+		g.db = sqlite3.connect(
+			DB_FILE,
+			detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+		)
+		g.db.row_factory = sqlite3.Row
+	return g.db
 
 def close_db():
 	""" Закрыть соединение с БД """
-	pass
+	db = g.pop('db', None)
+	if db is not None:
+		db.close()
